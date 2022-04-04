637c73643950e6b33c00662664362dba19744041
SergeySpiridonov <ser2000266@mail.ru>
2022-04-04 18:17:32 +0700

    Со временем закончил, осталось только убивать мобов на стадии казиныча

--- Assets/Scripts/Time/ChangeTime.cs
+++ Assets/Scripts/Time/ChangeTime.cs
@@ -8,18 +8,20 @@ public class ChangeTime : MonoBehaviour
     public Transform Rotate;
     public Animator up;
     public GameObject casinoWindow;
-    private float timeBattle = 90;
-    private bool flag = false;
+    private float speedTimeBattle = 360 / UIManager.timeBattle * 7f;
+    private float speedTimeCasino = 360/ UIManager.timeCasino * 7f;
     private float x;
+
     private void Start()
     {
-        StartCoroutine(RotationXFromTimeBattle());
-     //   up = GetComponentInChildren<Animator>();
+        StartCoroutine(RotationQuaternions());
+
     }
     void FixedUpdate()
     {
         CasinoOpen();
-        
+        // Debug.Log(speedTime);
+
     }
     //
     private void CasinoOpen()
@@ -27,43 +29,25 @@ public class ChangeTime : MonoBehaviour
         if (UIManager.timeBattle < 0 && UIManager.timeCasino > 0)
         {
             casinoWindow.SetActive(true);
-            if(UIManager.timeCasino < 3)
+            if (UIManager.timeCasino < 3)
                 up.SetBool("NightStart", true);
         }
         else
-        {   
+        {
             casinoWindow.SetActive(false);
         }
     }
-    //private IEnumerator AnimationUp()
-    //{
-    //    yield return new WaitForSeconds(up.cli);
-    //}
-    //ƒневное врем€(дл€ битвы)
-    private IEnumerator RotationXFromTimeBattle()
+
+    public IEnumerator RotationQuaternions()
     {
-        if (!flag)
+        yield return new WaitForSeconds(2f);
+        while (true)
         {
-            timeBattle = UIManager.timeBattle;
-            flag = true;
-        }
-        while (UIManager.timeBattle > 0)
-        {
-            x += Time.deltaTime * (100/timeBattle*15f);
-            transform.rotation = Quaternion.Euler(x, 0, 0);
-            yield return new WaitForSeconds(0.1f);
-        }
-    }
-    private IEnumerator RotationXFromTimeCasino()
-    {
-        if (!flag)
-        {
-            timeBattle = UIManager.timeBattle;
-            flag = true;
-        }
-        while (UIManager.timeBattle > 0)
-        {
-            x += Time.deltaTime * (100 / timeBattle * 15f);
+            if (!UIManager.flag)
+                x += Time.deltaTime * speedTimeBattle;
+
+            else
+                x -= Time.deltaTime * speedTimeCasino;
             transform.rotation = Quaternion.Euler(x, 0, 0);
             yield return new WaitForSeconds(0.1f);
         }
